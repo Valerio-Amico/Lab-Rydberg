9fe9b26b08829663ad2a80b5797a0da0876a2462
Valerio-Amico <valerio.amico98@gmail.com>
2022-12-05 15:36:38 +0100

    minors

--- open_quantum_systems/lib/twoatoms.py
+++ open_quantum_systems/lib/twoatoms.py
@@ -2,6 +2,7 @@ import qutip as qtp
 import numpy as np
 from arc import Rubidium87
 from scipy.constants import hbar
+import matplotlib.pyplot as plt
 
 class single_atom:
     def __init__(self, power_blue, waste_blue, power_IR, waste_IR, detuning_blue, power_depumping=None, waste_depumping=None, detuning_IR=0) -> None:
@@ -27,7 +28,7 @@ class single_atom:
         self.lower_transition_rabi_freq = Rubidium87().getRabiFrequency(n1=5,  l1=0, j1=0.5, mj1=0.5,  n2=6,  l2=1, j2=3/2, q=-1, laserPower=power_blue, laserWaist=waste_blue, s=0.5)*1e-6/(2*np.pi)
         self.higher_transition_rabi_freq = Rubidium87().getRabiFrequency(n1=70,  l1=0, j1=0.5, mj1=0.5,  n2=6,  l2=1, j2=3/2, q=-1, laserPower=power_IR, laserWaist=waste_IR, s=0.5)*1e-6/(2*np.pi)
         if power_depumping is not None and waste_depumping is not None:
-            self.depumping_rabi_freq = Rubidium87().getRabiFrequency(n1=70,  l1=0, j1=0.5, mj1=0.5,  n2=6,  l2=1, j2=3/2, q=-1, laserPower=power_depumping, laserWaist=waste_depumping, s=0.5)/(2*np.pi)
+            self.depumping_rabi_freq = Rubidium87().getRabiFrequency(n1=70,  l1=0, j1=0.5, mj1=0.5,  n2=6,  l2=1, j2=3/2, q=-1, laserPower=power_depumping, laserWaist=waste_depumping, s=0.5)*1e-6/(2*np.pi)
         else:
             self.depumping_rabi_freq = 0
         self.Gamma_6p = 1/Rubidium87().getStateLifetime(n=6, l=1, j=1.5, temperature=150*10**-6, includeLevelsUpTo=0, s=0.5)*1e-6 # in MHz
@@ -39,7 +40,7 @@ class single_atom:
             [0, 0, self.depumping_rabi_freq/2, -detuning_IR]
         ])
         def_i = 0
-        def_ry = 0
+        def_ry = 4.4
 
         S_minus_i = qtp.Qobj(self.Gamma_6p**(1/2)*(np.array([[0,1,0,1],[0,0,0,0],[0,0,0,0],[0,0,0,0]])))
         S_minus_ry = qtp.Qobj(self.Gamma_70s**(1/2)*np.array([[0,0,1,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
@@ -47,6 +48,8 @@ class single_atom:
         J_def_ry = qtp.Qobj(def_ry**(1/2)*np.array([[0,0,0,0],[0,0,0,0],[0,0,1,0],[0,0,0,0]]))
 
         self.Jump_ops = [S_minus_i, S_minus_ry, J_def_ry, J_def_i]
+        self.C6 = hbar * 2*np.pi * 870 *10**3
+        self.radius_blokade = (self.C6/(hbar * 2*np.pi * self.two_photon_rabi_freq))**(1/6)
         pass
 
     def evolve(self, time, steps, initial_DM, include_spontaneous_emission=True, include_defasing=False):
@@ -56,18 +59,54 @@ class single_atom:
             steps: number of steps
             initial_DM: initial density matrix (DM)
         """
+        if np.trace(initial_DM) != 1:
+            print("Initial density matrix must have trace equal to 1.")
+            raise
         times = np.linspace(0, time, steps)
+        if include_spontaneous_emission and include_defasing:
+            self.evo_results = qtp.mesolve(qtp.Qobj(self.H), qtp.Qobj(initial_DM), times, c_ops = self.Jump_ops)
+        elif include_spontaneous_emission:
+            self.evo_results = qtp.mesolve(qtp.Qobj(self.H), qtp.Qobj(initial_DM), times, c_ops = self.Jump_ops[0:2])
+        elif include_defasing:
+            self.evo_results = qtp.mesolve(qtp.Qobj(self.H), qtp.Qobj(initial_DM), times, c_ops = self.Jump_ops[2:4])
+        else:
+            self.evo_results = qtp.mesolve(qtp.Qobj(self.H), qtp.Qobj(initial_DM), times)
+        self.steps = steps
+        self.time = time
+        return
 
-        result = qtp.mesolve(qtp.Qobj(self.H), qtp.Qobj(initial_DM), times, c_ops = self.Jump_ops)
-        return result
+    def get_excitation_probability(self):
+        return np.real(np.trace(np.dot(self.evo_results.states[-1], projector(2,2))))
+    
+    def show(self):
+        evo = [np.array(self.evo_results.states[i]) for i in range(self.steps)]
+        p_trace_res = {}
+        labels = ["ground", "intermediate", "rydberg"]
+        for l, label in enumerate(labels):
+            if l==1: projector_ = projector(1,1) + projector(3,3)
+            else: projector_ = projector(l,l)
+            p_trace_res[label] = [np.trace(np.dot(evo[i], projector_)) for i in range(self.steps)]
+        plt.figure(figsize=(10,10))
 
+        times = np.linspace(0, self.time, self.steps)
+        for i, label in enumerate(labels):
+            plt.plot(times, p_trace_res[label], label=label)
+        plt.plot([0,times[-1]],[1,1],"--", alpha=0.5)
+        plt.plot([0,times[-1]],[0,0],"--", alpha=0.5)
+        plt.plot([0,times[-1]],[1/2,1/2],"--", alpha=0.5)
+        plt.ylim([0,max(p_trace_res["rydberg"])])
+        plt.title(label)
+        plt.legend()
+
+        plt.show()
+        return
 
 class two_atoms:
     def __init__(self, interatomic_distance, atom_L, atom_R) -> None:
         self.atom_L = atom_L
         self.atom_R = atom_R
         self.interatomic_distance = interatomic_distance
-        self.C6 = 870 *10**3
+        self.C6 = hbar * 2*np.pi * 870 *10**3
         self.Gamma_6p = 1/Rubidium87().getStateLifetime(n=6, l=1, j=1.5, temperature=150*10**-6, includeLevelsUpTo=0, s=0.5)*10**6 # in MHz
         self.Gamma_70s = 1/Rubidium87().getStateLifetime(n=70, l=0, j=0.5, temperature=150*10**-6, includeLevelsUpTo=0, s=0.5)*10**6 # in MHz
         self.V_f = self.V(self.interatomic_distance) # interaction potential
@@ -95,7 +134,7 @@ class two_atoms:
         return H_int
 
     def V(self, r): # r is in mircon and the energy is in Mhz
-        return self.C6/r**6
+        return self.C6/r**6 / (hbar*2*np.pi)
 
 def get_two_photon_rabi_freq(power_blue, waste_blue, power_IR, waste_IR, detuning_blue):
 
@@ -106,3 +145,8 @@ def get_two_photon_rabi_freq(power_blue, waste_blue, power_IR, waste_IR, detunin
     O6p_70s = atom.getRabiFrequency(n1=70,  l1=0, j1=0.5, mj1=0.5,  n2=6,  l2=1, j2=3/2, q=-1, laserPower=power_IR, laserWaist=waste_IR, s=0.5)*1e-6/(2*np.pi)
 
     return (O5s_6p*O6p_70s)/(detuning_blue)
+
+def projector(a,b):
+    projector_ = np.zeros([4, 4])
+    projector_[a,b] = 1
+    return projector_
